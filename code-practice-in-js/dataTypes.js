383004e98df914e77bc9d50e5f2d772c7ded159e
Navi Talib <talibali786.ta@gmail.com>
2023-04-15 17:32:51 +0530

    dataTypes

---
+++ code-practice-in-js/dataTypes.js
@@ -0,0 +1,34 @@
+//primitive data types in javaScript
+
+let x =161616; //numbers
+x= "hghjgjhdgfjhdg"; //string
+x= true; //boolean
+x =null; // null 
+x = undefined;
+
+// x= Symbol();
+//The JavaScript ES6 introduced a new primitive data type called Symbol. 
+//Symbols are immutable (cannot be changed) and are unique.
+
+
+//typeof is function which shows the datatypes of variables
+// console.log((typeof x)+":"+x)
+
+// Null in JavaScript means an empty value and is also a primitive type in JavaScript. 
+// The variable which has been assigned as null contains no value.
+// Undefined, on the other hand, means the variable has been declared, but its value has not been assigned.
+
+
+// Refrence data types in Js
+
+let mylist = [1,1,"12e6",9,"delhi"]; //array
+
+mylist = {
+    book:"C++",
+    rollno:22,
+    school:"nbbps",
+} //objects list
+
+mylist = new Date() //date
+
+console.log(mylist)
