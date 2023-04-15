bc51192f946b927963b18b734b6cd9a8e0889539
Navi Talib <talibali786.ta@gmail.com>
2023-04-15 16:52:39 +0530

    type conversion and coercion done

--- code-practice-in-js/typeCast.js
+++ code-practice-in-js/typeCast.js
@@ -1,5 +1,12 @@
 typeCast= (x)=>{
-console.log(x.toString());
+    // console.log((typeof Number(x))+":"+Number(x))  string to number type conversion
+// console.log((typeof x)+":"+x.toString());  number to string type conversion
+//  console.log((typeof parseInt(x))+":"+parseInt(x));  used for converting string to number
 }
 
-typeCast(55)
+typeCast("777")
+
+
+// Number() converts the type whereas parseInt parses the value of input.
+//  As you see, parseInt will parse up to the first non-digit character. 
+//  On the other hand, Number will try to convert the entire string.
