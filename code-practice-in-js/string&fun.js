d0f12059f67893e3cd073e5afa4da33156f7d1d3
Navi Talib <talibali786.ta@gmail.com>
2023-04-15 18:05:27 +0530

    string&fun

---
+++ code-practice-in-js/string&fun.js
@@ -0,0 +1,46 @@
+let str1 = "Greater Noida";
+let str2 = "New Delhi";
+
+// console.log(str1.length); returns length of a string
+
+
+//Extracting String Parts
+// slice(start, end)
+// console.log(str1.slice(7,13))
+// substring(start, end)
+// console.log(str1.substring(1,6))
+// substr(start, length)
+// console.log(str1.substr(7,13))
+
+// console.log(str1.toUpperCase())
+
+// console.log(str1.toLowerCase())
+
+// console.log(str1.substring(0,7).concat(" "+str2.substring(4,9)))
+// The concat() method joins two or more strings.
+
+// The concat() method does not change the existing strings.
+
+// The concat() method returns a new string.
+
+// console.log(str1.indexOf("Noida"))
+// console.log(str1.lastIndexOf("Greater"))
+
+// console.log(str1.charAt(3))
+
+// console.log(str1.startsWith('G'));
+
+// console.log(str1.endsWith('G'));
+
+// console.log(str1.includes('N'));
+
+// console.log(str1.split())
+// console.log(str1.split(''))
+
+//The split() method splits a string into an array of substrings.
+//The split() method returns the new array. The split() method does not change the original string ...
+
+console.log(str2.replace('New','Greater');)
+
+
+
