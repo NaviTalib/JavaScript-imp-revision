ba7aec3b3c3ecc054e2ae9c3ffc68094e117f10a
Navi Talib <talibali786.ta@gmail.com>
2023-04-16 19:48:59 +0530

    jsintprep

---
+++ code-practice-in-js/jsIntPrep.js
@@ -0,0 +1,40 @@
+// JavaScript Interview Question
+
+// // diff between == and ===
+// myFun =()=>{
+// let x = 3;
+// let y = "3";
+
+// if(x==y)
+// console.log(x==y) //  compares values
+// console.log(x===y); // compare values and types both
+// }
+// myFun()
+
+// //implicit conversion of data types
+// let x= 3;
+// let y ="2";
+// console.log(x-y)
+
+
+// functions
+
+// function declaration
+// function message(msg){
+//     console.log("This is message : "+msg)
+// };
+
+// message("hello")
+
+// function expression
+
+// let myfun = function(){
+//     console.log("This is function expression:")
+// }
+// myfun()
+
+// // anonymous function : fun
+
+// setTimeout(function(){
+//     console.log("anonymous function...")
+// },3000)
